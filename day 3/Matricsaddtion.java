dbcb92bdc897309bb2e771d5ae9c3e75ef03c929
Premkumar192211087 <123709732+Premkumar192211087@users.noreply.github.com>
2024-06-05 12:12:26 +0530

    Add files via upload

---
+++ day 3/Matricsaddtion.java
@@ -0,0 +1,12 @@
+class Matricsaddtion {
+    public static void main(String arr[]) {
+        int a[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int b[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++)
+        }
+        {
+            System.out.println(a[i][j] + b[i][j]);
+        }
+    }
+}
